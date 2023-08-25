52ad7557670f5689eb95051f4fbc3489ba274f2b
xxai.art <xxai.art@gmail.com>
2023-08-25 21:00:17 +0800

    ♨

---
+++ src/vps.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+VPS_LI="u1 u2 u3 uc sea mi m15"
+
+DIR=$(realpath $0) && DIR=${DIR%/*}
+cd $DIR
+set -ex
+
+rfp=/tmp/run.sh
+
+for vps in $VPS_LI; do
+  echo $vps
+
+  rsync -avz $DIR/run.sh $vps:$rfp
+  ssh $vps <<EOF
+    $rfp
+    rm -rf $rfp
+EOF
+
+done
