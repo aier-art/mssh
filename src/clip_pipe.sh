01cdcb4581a0ccb0e087d3e136717dfda298f3e7
xxai.art <xxai.art@gmail.com>
2023-08-29 22:52:40 +0800

    ♨

--- src/clip_pipe.sh
+++ src/clip_pipe.sh
@@ -3,7 +3,9 @@
 source /etc/profile
 set -ex
 cd ~/art/clip-runtime
-gitsync
+nc -z -w 1 127.0.0.1 7890 && export https_proxy=http://127.0.0.1:7890
+git fetch --all && git reset --hard origin/main
+#gitsync
 direnv allow
 cd clip_pipe
 pnpm i
