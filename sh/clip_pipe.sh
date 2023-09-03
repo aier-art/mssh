b861c3b090601d211e2079913b34eacd81e222cb
xxai.art <xxai.art@gmail.com>
2023-09-03 13:52:47 +0800

    ♨

--- sh/clip_pipe.sh
+++ sh/clip_pipe.sh
@@ -6,13 +6,15 @@ nc -z -w 1 127.0.0.1 7890 && export https_proxy=http://127.0.0.1:7890
 rustup update
 cd ~/art/conf
 gitsync
-cd ~/art/clip-runtime
+
+clip=~/art/clip-runtime
+cd $clip
 #git fetch --all && git reset --hard origin/main
 gitsync
 direnv allow
-cd ~/clip-runtime/rust/clip_img2qdrant
+cd $clip/rust/clip_img2qdrant
 ./dist.sh
-cd ~/art/clip_pipe
+cd $clip/clip_pipe
 pnpm i
 bunx cep -c src -o lib
 supervisorctl start xxai-clip || true
