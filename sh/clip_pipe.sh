dead7e678cb87e36e61682cdc4703fc5771c4825
xxai.art <xxai.art@gmail.com>
2023-09-03 13:49:00 +0800

    ♨

--- sh/clip_pipe.sh
+++ sh/clip_pipe.sh
@@ -3,13 +3,16 @@
 source /etc/profile
 set -ex
 nc -z -w 1 127.0.0.1 7890 && export https_proxy=http://127.0.0.1:7890
+rustup update
 cd ~/art/conf
 gitsync
 cd ~/art/clip-runtime
 #git fetch --all && git reset --hard origin/main
 gitsync
 direnv allow
-cd clip_pipe
+cd ~/clip-runtime/rust/clip_img2qdrant
+./dist.sh
+cd ~/art/clip_pipe
 pnpm i
 bunx cep -c src -o lib
 supervisorctl start xxai-clip || true
